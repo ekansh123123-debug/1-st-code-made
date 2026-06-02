1cfd9272f5b2403ab3df476d761047688962ad5c
ekansh-debug <ekansh123123@gmail.com>
2026-06-02 14:41:24 +0530

    removed std liabray

--- login_file.cpp
+++ login_file.cpp
@@ -1,8 +1,7 @@
 #include <iostream>
 #include <string>
 #include <fstream>
-using namespace std;
-bool email_cheak(const string &mail){
+bool email_cheak(const std::string &mail){
     bool condition1 = false, condition2 = false;
     size_t i = 0 ;
     while(i < mail.length())
@@ -25,10 +24,10 @@ bool email_cheak(const string &mail){
     }
     return (condition1 & condition2) && mail.length() > 5;
 }
-bool correct_username(const string &username){
-    fstream file;
-    string temp_username, temp_email_adress, temp_password;
-    file.open("loginid.txt", ios ::in);
+bool correct_username(const std::string &username){
+    std::fstream file;
+    std::string temp_username, temp_email_adress, temp_password;
+    file.open("loginid.txt", std:: ios ::in);
     if (file.is_open()){
     while (getline(file, temp_username, '*') && getline(file, temp_email_adress, '*') && getline(file, temp_password, '\n')){
         if (username == temp_username){
@@ -43,25 +42,26 @@ bool correct_username(const string &username){
 class Account
 {
 private:
-    string password, username, email_adress;
-    string temp_username, temp_email_adress, temp_password;
-    fstream file;
+    std::string password, username, email_adress;
+    std::string temp_username, temp_email_adress, temp_password;
+    std::fstream file;
 
 public:
     void login_function()
     {
         while (true)
         {
-            cout << "Enter username : ";
-            getline(cin, username);
-            cout << "Enter email adress : ";
-            getline(cin, email_adress);
-            cout << "Enter password : ";
-            getline(cin, password);
-            file.open("loginid.txt", ios ::in);
+            std::cout << "Enter username : ";
+            getline(std::cin, username);
+            std::cout << "Enter email adress : ";
+            getline(std::cin, email_adress);
+            std::cout << "Enter password : ";
+            getline(std::cin, password);
+            file.open("loginid.txt", std::ios ::in);
             if (!file.is_open())
             {
-                cout << "Could not open the file" << endl;
+                std::cout << "Could not open the file" <<std:: endl;
+                std::cout << "Try to register" << std::endl;
                 return;
             }
             while (getline(file, temp_username, '*') && getline(file, temp_email_adress, '*') && getline(file, temp_password, '\n'))
@@ -72,15 +72,15 @@ public:
                     break;
                 else
                 {
-                    cout << "Correct credentials" << endl;
-                    cout << "loging in ..." << endl;
+                    std::cout << "Correct credentials" <<std:: endl;
+                    std::cout << "loging in ..." << std::endl;
                     file.close();
                     // fuction of login
                     return;
                 }
             }
-            cout << "Incorrect credentials" << endl;
-            cout << "Try again" << endl;
+            std::cout << "Incorrect credentials" <<std:: endl;
+            std::cout << "Try again" << std::endl;
             file.close();
         }
     }
@@ -88,61 +88,61 @@ public:
     {
         while (true)
         {
-            cout << "Enter username : ";
-            getline(cin, temp_username);
+            std::cout << "Enter username : ";
+            getline(std::cin, temp_username);
             if (!correct_username(temp_username))
             {
-                cout << "This name is already taken or incorrect length" << endl;
-                cout << "Please try another username " << endl;
+                std::cout << "This name is already taken or incorrect length" << std::endl;
+                std::cout << "Please try another username " << std::endl;
                 continue;
             }
 
-            cout << "Enter email adress : ";
-            getline(cin, temp_email_adress);
+            std::cout << "Enter email adress : ";
+            getline(std::cin, temp_email_adress);
             if (!email_cheak(temp_email_adress))
             {
-                cout << "Incorrect email format " << endl;
-                cout << "Try again " << endl;
+                std::cout << "Incorrect email format " << std::endl;
+                std::cout << "Try again \n" << std::endl;
                 continue;
             }
 
-            cout << "Enter password : ";
-            getline(cin, temp_password);
+            std::cout << "Enter password : ";
+            getline(std::cin, temp_password);
             if (temp_password.length() < 6)
             {
-                cout << "Try a longer password" << endl;
+                std::cout << "Try a longer password" << std::endl;
                 continue;
             }
             break;
         }
 
-        file.open("loginid.txt", ios ::out | ios::app);
-        file << temp_username << "*" << temp_email_adress << "*" << temp_password << endl;
+        file.open("loginid.txt", std::ios ::out | std::ios::app);
+        file << temp_username << "*" << temp_email_adress << "*" << temp_password << std::endl;
 
         file.close();
 
         if (file.is_open())
         {
-            cout << "Failed to register " << endl;
-            cout << "Try again later ..." << endl;
+            std::cout << "Failed to register " <<std:: endl;
+            std::cout << "Try again later ..." << std::endl;
             return;
         }
 
-        cout << "Registration Sucessfull";
+        std::cout << "Registration Sucessfull";
     }
     void forgot_password()
     {
 
-        cout << "Enter username : ";
-        getline(cin, username);
+        std::cout << "Enter username : ";
+        getline(std::cin, username);
 
-        cout << "Enter email adress : ";
-        getline(cin, email_adress);
+        std::cout << "Enter email adress : ";
+        getline(std::cin, email_adress);
 
-        file.open("loginid.txt", ios ::in);
+        file.open("loginid.txt", std::ios ::in);
         if(!file.is_open()){
-            cout << "Errer in opening the file " << endl;
-            cout << "Try to register" << endl;
+            std::cout << "Errer in opening the file " << std::endl;
+            std::cout << "Try to register" << std::endl;
             return;
         }
 
@@ -150,19 +150,19 @@ public:
             if (username == temp_username)
             {
                 if (temp_email_adress == email_adress){
-                    cout << "Password is : " << temp_password << endl;
+                    std::cout << "Password is : " << temp_password << std::endl;
                     file.close();
                     return;
                 }
                 else{
-                    cout << "username does not match email adress ..." << endl;
-                    cout << "Pleas try again or register " << endl;
+                    std::cout << "username does not match email adress ..." <<std:: endl;
+                    std::cout << "Pleas try again or register " << std::endl;
                     file.close();
                     return;
                 }
             }
         }
-        cout << "Could not find username\nTry again or register " << endl;
+        std::cout << "Could not find username\nTry again or register " << std::endl;
         file.close();
     }
 } obj;
@@ -171,34 +171,32 @@ int main()
     Account a1;
     while(true)
         {
-            cout << "Select" << endl;
-            cout << "1. Login" << endl;
-            cout << "2. Register" << endl;
-            cout << "3. Forgot Password" << endl;
-            cout << "4. Exit" << endl;
+            std::cout << "Select" << std::endl;
+            std::cout << "1. Login" << std::endl;
+            std::cout << "2. Register" << std::endl;
+            std::cout << "3. Forgot Password" << std::endl;
+            std::cout << "4. Exit" << std::endl;
             int cas;
-            cin >> cas;
+            std::cin >> cas;
             switch (cas)
             {
             case 1:
-                cin.ignore();
+                std::cin.ignore();
                 obj.login_function();
                 return 0;
             case 2:
-                cin.ignore();
+                std::cin.ignore();
                 obj.registe_function();
                 break;
             case 3:
-                cin.ignore();
+                std::cin.ignore();
                 obj.forgot_password();
                 break;
             case 4:
                 return 0;
                 break;
-
             default:
-                cout << "invalid input \nTry again" << endl;
-
+                std::cout << "invalid input \nTry again" << std::endl;
                 break;
             }
         }
